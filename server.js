0e97fa2f106adf3968a08fbf9aa631ae6a7fd3fa
vctormarques <victor.marques17@gmail.com>
2021-08-11 09:47:13 -0300

    Alterado porta do server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8181
 const server = http.createServer(app);
 server.listen(port);
